73a6585e2106dc74c9c6dc08eee641f0f1af55fe
Felix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-05-08 10:05:46 -0400

    working

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -1,18 +1,36 @@
-import { Button, Card } from "react-bootstrap"
+import PropTypes from 'prop-types';
+import { Button, Card } from "react-bootstrap";
+import getState from '../resources/Flux';
 
-const MiniCard = ({ props }) => {
+const MiniCard = ({ element }, { state }) => {
+
+    const handlerFavButton = () => {
+        const storeCopy = state.store;
+        storeCopy.Favoritos.some((element) => element.name === data.name)
+            ? setStore({
+                Favoritos: storeCopy.Favoritos.filter(
+                    (element) => element.name !== data.name
+                ),
+            })
+            : setStore({ Favoritos: [...storeCopy.Favoritos, data] })
+    };
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
+                <Card.Title>{element.name}</Card.Title>
                 <Card.Text>
                     Leer Mas...
                 </Card.Text>
                 <Button variant="outline-secondary">Detalles</Button>
-                <Button variant="outline-danger"><i className="bi bi-bookmark-heart" ></i></Button>
+                <Button variant="outline-danger" onClick={handlerFavButton(element)}><i className="bi bi-bookmark-heart" ></i></Button>
             </Card.Body>
         </Card>
-    )
-}
+    );
+};
 
-export default MiniCard
+export default MiniCard;
+
+MiniCard.propTypes = {
+    element: PropTypes.object,
+    actions: PropTypes.object,
+};
